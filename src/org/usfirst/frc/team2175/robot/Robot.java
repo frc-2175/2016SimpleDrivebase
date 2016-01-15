95bb013f22c8c8fb751e47ebdc584b28cfbeabd0
Max Haland <max@haland.org>
2016-01-15 16:25:47 -0600

    Change drive code to add scaling
    For some reason, this does not work on the robot for reasons I am
    unable to fathom, and needs further testing. In addition, the file
    was cleaned with workspace mechanic

--- src/org/usfirst/frc/team2175/robot/Robot.java
+++ src/org/usfirst/frc/team2175/robot/Robot.java
@@ -19,70 +19,88 @@ public class Robot extends IterativeRobot {
     final String customAuto = "My Auto";
     String autoSelected;
     SendableChooser chooser;
-    
+
     RobotDrive drivetrain;
-	Joystick leftStick;
-	Joystick rightStick;
-    
+    Joystick leftStick;
+    Joystick rightStick;
+
+    double moveValue;
+    double turnValue;
+
+    double speedScale;
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+    @Override
     public void robotInit() {
         chooser = new SendableChooser();
         chooser.addDefault("Default Auto", defaultAuto);
         chooser.addObject("My Auto", customAuto);
         SmartDashboard.putData("Auto choices", chooser);
-        
-        drivetrain = new RobotDrive(0,1);
+
+        drivetrain = new RobotDrive(2, 3);
         leftStick = new Joystick(0);
         rightStick = new Joystick(1);
+
+        speedScale = 1;
     }
-    
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
-	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
-	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
-	 * below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
-	 * If using the SendableChooser make sure to add them to the chooser code above as well.
-	 */
+
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable
+     * chooser code works with the Java SmartDashboard. If you prefer the
+     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+     * getString line to get the auto name from the text box below the Gyro
+     *
+     * You can add additional auto modes by adding additional comparisons to the
+     * switch structure below with additional strings. If using the
+     * SendableChooser make sure to add them to the chooser code above as well.
+     */
+    @Override
     public void autonomousInit() {
-    	autoSelected = (String) chooser.getSelected();
-//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
+        autoSelected = (String) chooser.getSelected();
+        // autoSelected = SmartDashboard.getString("Auto Selector",
+        // defaultAuto);
+        System.out.println("Auto selected: " + autoSelected);
     }
 
     /**
      * This function is called periodically during autonomous
      */
+    @Override
     public void autonomousPeriodic() {
-    	switch(autoSelected) {
-    	case customAuto:
-        //Put custom auto code here   
+        switch (autoSelected) {
+        case customAuto:
+            // Put custom auto code here
             break;
-    	case defaultAuto:
-    	default:
-    	//Put default auto code here
+        case defaultAuto:
+        default:
+            // Put default auto code here
             break;
-    	}
+        }
     }
 
     /**
      * This function is called periodically during operator control
      */
+    @Override
     public void teleopPeriodic() {
-    	while(isOperatorControl() && isEnabled()){
-    		drivetrain.arcadeDrive(leftStick.getY(), rightStick.getX());
-    	}
+        moveValue = leftStick.getY() * speedScale;
+        turnValue = rightStick.getX() * speedScale;
+
+        while (isOperatorControl() && isEnabled()) {
+            drivetrain.arcadeDrive(moveValue, turnValue);
+        }
     }
-    
+
     /**
      * This function is called periodically during test mode
      */
+    @Override
     public void testPeriodic() {
-    
+
     }
-    
+
 }
