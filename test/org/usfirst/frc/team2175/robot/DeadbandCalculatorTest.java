91bb82d41dd59ad9351578da7786fffe42c0e1f1
Max Haland <max@haland.org>
2016-01-16 18:20:22 -0600

    Add test package

---
+++ test/org/usfirst/frc/team2175/robot/DeadbandCalculatorTest.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team2175.robot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeadbandCalculatorTest {
+
+    @Test
+    public void testCalcLinearControlCurveSlope() throws Exception {
+        // TODO implement me
+    }
+
+}
